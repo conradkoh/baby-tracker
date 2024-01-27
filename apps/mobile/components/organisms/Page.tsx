8ddf512c89b855ef8c6d457f2bfedecfe0ace404
conradkoh <conradkoh@gmail.com>
2024-01-27 10:37:53 +0800

    styles: ensure that scrolling only happens when content overflows

--- apps/mobile/components/organisms/Page.tsx
+++ apps/mobile/components/organisms/Page.tsx
@@ -13,15 +13,15 @@ export default function Page(props: { title: string; children: any }) {
         reset: () => setBottomEl(null),
       }}
     >
-      <View className="p-2 flex-1 bg-red-500 min-h-screen">
+      <View className="p-2 flex-1 bg-blue-200 min-h-screen">
         <View className="h-30">
           <Text className="mt-4 text-3xl font-bold">{props.title}</Text>
           <View className="my-2">
             <AppNav />
           </View>
         </View>
-        <View className="flex-1 bg-gray-100">
-          <ScrollView>
+        <View className="flex-1">
+          <ScrollView alwaysBounceVertical={false}>
             <View>{props.children}</View>
           </ScrollView>
         </View>
