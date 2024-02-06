349c1cc4bb8df68b09f43411d296b55ea9d170e6
conradkoh <conradkoh@gmail.com>
2024-02-07 00:20:18 +0800

    added branching on mobile and deploy for backend

--- apps/mobile/ConvexClientProvider.tsx
+++ apps/mobile/ConvexClientProvider.tsx
@@ -1,7 +1,7 @@
 'use client';
-import { ReactNode, useMemo, useState } from 'react';
+import { ReactNode, useMemo } from 'react';
 import { ConvexProvider, ConvexReactClient } from 'convex/react';
-import { Branch } from './branch';
+import { Branch, useActiveBranch } from './branch';
 
 const convexDev = new ConvexReactClient(
   process.env.EXPO_PUBLIC_CONVEX_URL_DEV!
@@ -15,8 +15,7 @@ export default function ConvexClientProvider({
 }: {
   children: ReactNode;
 }) {
-  // const [client, setClient] = useState(convexProd);
-  const [branch, setBranch] = useState(Branch.Prod);
+  const { branch } = useActiveBranch();
   const client = useMemo(() => {
     let client = convexProd;
     if (branch === Branch.Dev) {
--- apps/mobile/branch.ts
+++ apps/mobile/branch.ts
@@ -1,4 +1,18 @@
+import { useMemo } from 'react';
+
 export enum Branch {
   Dev = 'dev',
   Prod = 'prod',
+}
+export function useActiveBranch() {
+  const env = process.env.NODE_ENV;
+  return useMemo(() => {
+    return { branch: getBranch(env) };
+  }, [env]);
+}
+export function getBranch(emv: string | undefined): Branch {
+  if (emv === 'development') {
+    return Branch.Dev;
+  }
+  return Branch.Prod;
 }
