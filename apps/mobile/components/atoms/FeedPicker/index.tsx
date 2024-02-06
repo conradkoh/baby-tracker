a2344cfbeb73ed92c2a948f0b05dfe29f8dd0858
conradkoh <conradkoh@gmail.com>
2024-02-07 02:32:24 +0800

    refactored SelectPicker

--- apps/mobile/components/atoms/FeedPicker/index.tsx
+++ apps/mobile/components/atoms/FeedPicker/index.tsx
@@ -1,80 +1,21 @@
-import React, { useState } from 'react';
-import {
-  View,
-  Button,
-  Modal,
-  StyleSheet,
-  TouchableOpacity,
-  Text,
-  Platform,
-  Pressable,
-  SafeAreaView,
-} from 'react-native';
-import { Picker } from '@react-native-picker/picker';
-const viewValues = new Map([
-  ['expressed', 'Expressed'],
-  ['latch', 'Latch'],
-  ['formula', 'Formula'],
-]);
+import React from 'react';
+import SelectPicker from '../SelectPicker';
+
 const FeedPicker = (props: {
   value: string;
   onChange: (v: string) => void;
 }) => {
-  const [visible, setVisible] = useState(false);
-  // const [selectedValue, setSelectedValue] = useState('expressed');
-
   return (
-    <>
-      <View className="w-full flex items-center rounded">
-        <View
-          className=" place-self-center w-auto rounded-md p-2"
-          style={{ backgroundColor: 'rgba(184, 207, 237, 255)' }}
-        >
-          <Pressable onPress={() => setVisible(true)}>
-            <Text>{viewValues.get(props.value)}</Text>
-          </Pressable>
-        </View>
-      </View>
-      <View className="flex-1 justify-center align-middle">
-        <Modal
-          animationType="slide"
-          transparent={true}
-          visible={visible}
-          onRequestClose={() => setVisible(false)}
-        >
-          <View className="absolute bottom-0 w-full bg-slate-50 rounded-t-lg px-2 py-1 align-middle justify-center">
-            {Platform.OS === 'ios' && (
-              <TouchableOpacity
-                style={styles.doneButton}
-                onPress={() => setVisible(false)}
-              >
-                <Text>Done</Text>
-              </TouchableOpacity>
-            )}
-            <Picker
-              selectedValue={props.value}
-              onValueChange={(itemValue) => props.onChange(itemValue)}
-              style={{ width: '100%' }}
-            >
-              <Picker.Item
-                label={viewValues.get('expressed')}
-                value="expressed"
-              />
-              <Picker.Item label={viewValues.get('formula')} value="formula" />
-              <Picker.Item label={viewValues.get('latch')} value="latch" />
-            </Picker>
-          </View>
-        </Modal>
-      </View>
-    </>
+    <SelectPicker
+      options={[
+        { value: 'expressed', label: 'Expressed' },
+        { value: 'latch', label: 'Latch' },
+        { value: 'formula', label: 'Formula' },
+      ]}
+      value={props.value}
+      onChange={props.onChange}
+    />
   );
 };
-
-const styles = StyleSheet.create({
-  doneButton: {
-    alignSelf: 'flex-end',
-    margin: 10,
-  },
-});
 
 export default FeedPicker;
---
+++ apps/mobile/components/atoms/SelectPicker.tsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import {
+  View,
+  Button,
+  Modal,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  Platform,
+  Pressable,
+  SafeAreaView,
+} from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+
+const SelectPicker = (props: {
+  value: string;
+  options: {
+    value: any;
+    label: string;
+  }[];
+  onChange: (v: string) => void;
+}) => {
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <>
+      <View className="w-full flex items-center rounded">
+        <View
+          className=" place-self-center w-auto rounded-md p-2"
+          style={{ backgroundColor: 'rgba(184, 207, 237, 255)' }}
+        >
+          <Pressable onPress={() => setVisible(true)}>
+            <Text>
+              {props.options.find((o) => o.value === props.value)?.label ||
+                'Unknown'}
+            </Text>
+          </Pressable>
+        </View>
+      </View>
+      <View className="flex-1 justify-center align-middle">
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={visible}
+          onRequestClose={() => setVisible(false)}
+        >
+          <View className="absolute bottom-0 w-full bg-slate-50 rounded-t-lg px-2 py-1 align-middle justify-center">
+            {Platform.OS === 'ios' && (
+              <TouchableOpacity
+                style={styles.doneButton}
+                onPress={() => setVisible(false)}
+              >
+                <Text>Done</Text>
+              </TouchableOpacity>
+            )}
+            <Picker
+              selectedValue={props.value}
+              onValueChange={(itemValue) => {
+                console.log({ itemValue });
+                return props.onChange(itemValue);
+              }}
+              style={{ width: '100%' }}
+            >
+              {props.options.map((o) => (
+                <Picker.Item key={o.value} value={o.value} label={o.label} />
+              ))}
+            </Picker>
+          </View>
+        </Modal>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  doneButton: {
+    alignSelf: 'flex-end',
+    margin: 10,
+  },
+});
+
+export default SelectPicker;
