09ed9550bcc33adf4fd65ff02d6ea83cd3c6f25d
conradkoh <conradkoh@gmail.com>
2024-02-10 01:00:26 +0800

    attempt to increase auto update reliability

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -4,7 +4,25 @@ import ConvexClientProvider from '../ConvexClientProvider';
 import { Slot } from 'expo-router';
 import AppDataProvider from '../providers/AppDataProvider';
 import { NativeWindStyleSheet } from 'nativewind';
+import * as Updates from 'expo-updates';
+import { useEffect } from 'react';
 export default function RootLayout() {
+  async function onFetchUpdateAsync() {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    } catch (error) {
+      // You can also add an alert() to see the error message in case of an error when fetching updates.
+      alert(`Error fetching latest Expo update: ${error}`);
+    }
+  }
+  useEffect(() => {
+    setInterval(() => onFetchUpdateAsync(), 1000 * 60);
+  }, []);
   return (
     <ConvexClientProvider>
       <AppDataProvider>
