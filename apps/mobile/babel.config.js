1b9eb1396234b2a818fedba07b8d2c281f9b6b50
conradkoh <conradkoh@gmail.com>
2024-03-01 09:27:51 +0800

    disable babel cache and fix tailwind config causing non-std c++ crash

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-undef
 module.exports = function (api) {
-  api.cache(true);
+  api.cache(false);
   return {
     presets: ['babel-preset-expo'],
     plugins: ['nativewind/babel'],
--- apps/mobile/tailwind.config.js
+++ apps/mobile/tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
     './screens/**/*.{js,jsx,ts,tsx}',
     './app/**/*.{js,jsx,ts,tsx}',
     './components/**/*.{js,jsx,ts,tsx}',
-    './**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
